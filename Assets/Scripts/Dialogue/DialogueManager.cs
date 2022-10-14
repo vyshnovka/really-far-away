4f633501c47da2c256704512c51a8c530a344079
Vykula <viktoria.zheneral@gmail.com>
2022-10-14 21:57:27 +0300

    bug with dialogue fixed

--- Assets/Scripts/Dialogue/DialogueManager.cs
+++ Assets/Scripts/Dialogue/DialogueManager.cs
@@ -68,6 +68,8 @@ public class DialogueManager : MonoBehaviour
                     }
                     else
                     {
+                        dialogueToDisplay = null;
+
                         objectOfInteraction.FinishInteraction();
                     }
                 }
