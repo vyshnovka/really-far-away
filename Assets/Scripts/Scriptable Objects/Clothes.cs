2f829209b1f30ac1230aa036b99e356ee51814b6
Vykula <viktoria.zheneral@gmail.com>
2022-10-14 16:49:23 +0300

    scriptable objects for shop created

---
+++ Assets/Scripts/Scriptable Objects/Clothes.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.U2D.Animation;
+
+public enum ClothesType
+{
+    Shirt,
+    Pants,
+    Shoes,
+    MakeUp
+}
+
+[CreateAssetMenu(fileName = "Clothes", menuName = "Scriptable Objects/Clothes")]
+public class Clothes : ScriptableObject
+{
+    public string description;
+    public ClothesType type;
+    public Sprite icon;
+    public int price;
+    public SpriteLibraryAsset sprites;
+}
